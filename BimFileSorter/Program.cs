9dc60e08ba70ae4f9359ea7e096f6b27806822a4
peter <pete_bomber@hotmail.com>
2018-09-17 21:48:43 +1200

    Initial commit

---
+++ BimFileSorter/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace BimFileSorter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.Error.WriteLine("Usage: formatjson.exe <jsonfile>");
+                return;
+            }
+
+            var filename = args[0];
+            var path = Path.Combine(Directory.GetCurrentDirectory(), filename);
+            if (!File.Exists(path))
+            {
+                Console.Error.WriteLine($"File {path} does not exist");
+                return;
+            }
+
+            var content = File.ReadAllText(path);
+
+            if (!(JsonConvert.DeserializeObject(content) is JObject jObject))
+            {
+                Console.Error.WriteLine($"Not a valid JSON object: {path}");
+                return;
+            }
+
+            Sort(jObject);
+
+            File.WriteAllText(path, JsonConvert.SerializeObject(jObject, Formatting.Indented));
+        }
+
+        private static void Sort(JObject rootJObject)
+        {
+            var model = (JObject)rootJObject["model"];
+
+            SortArray(model, "tables", "name");
+            foreach (JObject table in rootJObject["model"]["tables"])
+            {
+                SortArray(table, "columns", "name");
+                SortArray(table, "measures", "name");
+            }
+
+            SortArray(model, "relationships", "name");
+        }
+
+        private static void SortArray(JObject container, string arrayName, string sortPropertyName)
+        {
+            var array = container[arrayName] as JArray;
+            if (array == null)
+            {
+                // Not an error if the property is missing.
+                return;
+            }
+
+            var properties = container.Properties().ToList();
+            foreach (var property in properties)
+            {
+                // It's apparently not possible to re-order an array in-place,
+                // so we have to remove it and re-add the property with the sorted
+                // array. We remove and re-add *every* property so that they remain
+                // in the same order.
+                property.Remove();
+                if (property.Name == arrayName)
+                {
+                    property.Value = new JArray(array.OrderBy(o => (string)o[sortPropertyName]));
+                }
+
+                container.Add(property);
+            }
+        }
+    }
+}
---
+++ BimFileSorter/README.md
@@ -0,0 +1,46 @@
+﻿# BIM file JSON sorter
+
+Ensures a consistent ordering of a number of JSON arrays within a PowerBI `BIM` file.
+
+This is to enable easier comparisons between versions of these files, and reduce the likelihood of merge conflicts.
+
+The specific array properties which are re-ordered are:
+
+```
+{
+  "model": {
+    "tables": [
+      {
+        "name": "<tables are ordered by name>",
+        "columns": [
+          {
+            "name": "<columns are ordered by name>",
+            ...
+          }
+        ],
+        "measures": [
+          {
+            "name": "<measures are ordered by name>",
+            ...
+          }
+        ],
+        ...
+      },
+      ...
+    ],
+    "relationships": [
+      {
+        "name": "<relationships are ordered by name>",
+        ...
+      },
+      ...
+    ],
+    ...
+  },
+  ...
+}
+```
+
+## Building
+
+Uses [Costura](https://github.com/Fody/Costura) to package all dependencies into a single executable, `formatjson.exe`, for easier distribution.
